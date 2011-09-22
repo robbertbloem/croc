b36e3054c24725347e98e3aa97ca0285830b3c01
Robbert <robbertbloem@gmail.com>
2011-09-22 10:32:23 +0200

    When printing the class, 3 dimensional arrays will be displayed correctly.
    For 3 dimensional arrays, only the first two dimensions were written.
    Now the first three dimensions are written. For >3 dimensional arrays,
    it will print some dots to indicate that not all is printed.

--- DataClasses.py
+++ DataClasses.py
@@ -275,15 +275,22 @@ def format_print(var):
     """
     # list
     if type(var) == list:
-        typ=range(len(var))       
+        typ = range(len(var))       
         for i in range(0, len(var)):
-            typ[i]=(format_print(var[i]))
+            typ[i] = (format_print(var[i]))
         return typ
     # ndarray
     elif type(var) == numpy.ndarray:
-        a=numpy.shape(var)
-        if len(a) == 1: return (str(a[0]) + " x 1")
-        else: return str(a[0]) + " x " + str(a[1])
+        a = numpy.shape(var)
+        if len(a) == 1: 
+            return str(a[0]) + " x 1"
+        elif len(a) == 2:
+            return str(a[0]) + " x " + str(a[1])
+        elif len(a) == 3:
+            return str(a[0]) + " x " + str(a[1]) + " x " + str(a[2])
+
+        else: 
+            return str(a[0]) + " x " + str(a[1]) + " x " + str(a[2]) + " x ..."
     # time
     elif type(var) == time.struct_time: 
         var = time.strftime("%a, %d %b %Y %H:%M:%S", var)
