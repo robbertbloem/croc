b5d903edc18cfedc3e88d1548c452c59b374007f
Robbert <robbertbloem@gmail.com>
2012-02-23 09:44:15 +0100

    Added some documentation and a better formatting of a table

--- Concentrations.py
+++ Concentrations.py
@@ -8,11 +8,33 @@ import matplotlib.pyplot as plt
 import Resources.Mathematics as M
 
 
-def concentration_vs_percentage_bound(kd, concentration_ratio = [0,5], n_steps = 100, flag_new_plot = True):
+def concentration_vs_percentage_bound(kd, concentration_ratio = [0,5], print_for = [], n_steps = 100):
     """
-    Calculates the percentage bound as a function of concentration (ratio) for a given Kd. 
+    Calculates the percentage bound as a function of concentration (ratio) for a given Kd for: A+B <=> AB.
+    The concentration of A is fixed to 1, the 'concentration_ratio' gives the range of concentrations of B.
+    
+    CHANGELOG:
+    201111??/RB: Wrote original function. 
+    20120222/RB: Integrated it into croc.
+    
+    INPUT:
+    - kd (int or array): the dissociation constant. Kd = 1/Ka
+    - concentration_ratio (array with two elements): the minimum and maximum of the range over which it should be calculated
+    - print_for (array or int): print the values for these concentrations.
+    - n_steps (int): number of steps for the calculation
+    
+    OUTPUT:
+    - a graph.
+    - if print_for is given, a list with percentage bound for different concentrations for B for different values of Kd.
+    
     
     """
+    
+    if type(kd) == float or type(kd) == int:
+        kd = [kd]
+    
+    if type(print_for) == float or type(print_for) == int:
+        print_for = [print_for]
 
     x0 = 1
     y0 = numpy.arange(1, n_steps + 1) * (concentration_ratio[1] - concentration_ratio[0])/n_steps
@@ -23,32 +45,49 @@ def concentration_vs_percentage_bound(kd, concentration_ratio = [0,5], n_steps =
     
     r = numpy.zeros(n_steps)
     
-    for i in range(n_steps):
-        a = 1
-        b = -(x0 + y0[i] + kd)
-        c = x0 * y0[i]
-        
-        xy[0], xy[1] = M.square_formula(a, b, c)
-        
-        for j in range(2):
-            x[j] = x0 - xy[j]
-            y[j] = y0[i] - xy[j]
-            
-            if x[j] < 0 or y[j] < 0:
-                pass
-            else:
-                r[i] = 100 - 100*y[j]/y0[i]
+    plt.figure()
     
-    if flag_new_plot:       
-        plt.figure()
-    plt.plot(y0/x0, r)
+    for k in range(len(kd)):
+    
+        for i in range(n_steps):
+            a = 1
+            b = -(x0 + y0[i] + kd[k])
+            c = x0 * y0[i]
+            
+            xy[0], xy[1] = M.square_formula(a, b, c)
+            
+            for j in range(2):
+                x[j] = x0 - xy[j]
+                y[j] = y0[i] - xy[j]
+                
+                if x[j] < 0 or y[j] < 0:
+                    pass
+                else:
+                    r[i] = 100 - 100*y[j]/y0[i]
+
+        plt.plot(y0/x0, r, label = str(kd[k]))
+
+        if len(print_for) != 0:
+            print("For Kd: " + str(kd[k]))
+            for i in range(len(print_for)):
+                index = numpy.where(y0/x0 >= print_for[i])
+                print(str((y0/x0)[index[0][0]]) + ": " + str(numpy.round(r[index[0][0]],1)) + "%")
+
+        
+    plt.title("Percentage bound as a function of concentration and Kd\nA+B <=> AB")
+    plt.ylim(0,100)
+    plt.xlabel("Concentration B/A")
+    plt.ylabel("Percentage bound (%)")
+    plt.legend()
     plt.show()
+    
+
     
 
 
 
 if __name__ == "__main__": 
-    plt.figure()
-    concentration_vs_percentage_bound(0.01, flag_new_plot = False)
-    concentration_vs_percentage_bound(0.03, flag_new_plot = False)
+
+    concentration_vs_percentage_bound([0.02,0.01], print_for = 1)
+
     
--- GroupVelocityDispersion.py
+++ GroupVelocityDispersion.py
@@ -15,12 +15,34 @@ reload(Resources.GVD_Materials)
 
 
 
-def GVD(material, print_for_um = [4], range_um = [1,10], material_path_mm = 0, pulse_length_fs = 0, n_steps = 100, flag_plot_n = True, flag_plot_gvd = True, y_range_gvd = [0,0]):
+def GVD(material, print_for_um = [], range_um = [1,10], material_path_mm = 0, pulse_length_fs = 0, n_steps = 100, flag_plot_n = True, flag_plot_gvd = True, y_range_gvd = [0,0]):
     """
     croc.GroupVelocityDispersion.GVD calculates the GVD for a certain material in a certain wavelength range using the Sellmeier equation.
     
+    CHANGELOG:
+    201109??/RB: started the method.
+    20120222/RB: integrated it into croc.
+    
+    INPUT:
+    - material (string): the name of the material. The full list of names can be found in croc.Resources.GVD_Materials
+    - print_for_um (int or array): print the GVD for these wavelengths, in micron
+    - range_um (array, 2 elements): minimum and maximum of the calculation in micron. It should be between ~1 and ~10.
+    - material_path_mm (int, default = 0): Path through material in mm. If >0 and pulse_lenth_fs > 0, it calculates the effect on the pulse length for the values in print_for_um
+    - pulse_length_fs (int, default = 0): Pulse length in fs. If >0 and material_path_mm > 0, it calculates the effect on the pulse length for the values in print_for_um
+    - flag_plot_n (BOOL, True): plot the index of refraction for range_um
+    - flag_plot_gvd (BOOL, True): plot the gvd for range_um
+    - y_range_gvd (array, 2 elements): Give the range of GVD that is plotted. Default is [0,0], which will give the full range.
+    
+    OUTPUT:
+    - a graph of the GVD if flag_plot_gvd == True
+    - a graph of the index of refraction if flag_plot_n == True
+    - values of GVD for the wavelength given in print_for_um
+    - if pulse_length_fs and material_path_mm are >0, it will also give the effect on the pulse length
+    
     """
     
+    if type(print_for_um) == float or type(print_for_um) == int:
+        print_for_um = [print_for_um]
     
     # import properties
     [n, SC] = Resources.GVD_Materials.MaterialProperties(material)
@@ -33,20 +55,30 @@ def GVD(material, print_for_um = [4], range_um = [1,10], material_path_mm = 0, p
         if print_for_um != []:
             print(material)
             
-            if material_path_mm > 0:
+            if material_path_mm > 0 and pulse_length_fs > 0:
                 print(str(material_path_mm) + " mm path")
-                print("um \tGVD (fs^2/mm)\t" + str(pulse_length_fs) + " fs")
+                print("{0:4} {1:14} {2:5}".format("um", "GVD (fs^2/mm)", str(pulse_length_fs) + " fs"))
                 for i in range(len(print_for_um)):
-                    index = numpy.where(gvd_x >= print_for_um[i])
-                    new_pulse_length_fs = F.calculate_effect_GVD(pulse_length_fs, gvd_y[index[0][0]]*material_path_mm)
-                    print(str(numpy.round(gvd_x[index[0][0]], 2)) + "\t" + str(numpy.round(gvd_y[index[0][0]], 3)) + "   \t" + str(numpy.round(new_pulse_length_fs, 1)))
+                    if print_for_um[i] > range_um[0] and print_for_um[i] < range_um[1]:
+                        index = numpy.where(gvd_x >= print_for_um[i])
+                        new_pulse_length_fs = F.calculate_effect_GVD(pulse_length_fs, gvd_y[index[0][0]]*material_path_mm)
+                        s_a = str(numpy.round(gvd_x[index[0][0]], 2))
+                        s_b = str(numpy.round(gvd_y[index[0][0]], 3))
+                        s_c = str(numpy.round(new_pulse_length_fs, 1))
+                        print("{0:4} {1:14} {2:5}".format(s_a.rjust(4), s_b.rjust(13), s_c.rjust(5)))
+                    else:
+                        print("ERROR (croc.GroupVelocityDispersion.GroupVelocityDispersion): Can not print value for wavelength that is out of range of the calculation.")
                     
             else:
-                print("um \tGVD (fs^2/mm)")
+                print("{0:4} {1:14}".format("um", "GVD (fs^2/mm)"))
                 for i in range(len(print_for_um)):
-                    index = numpy.where(gvd_x >= print_for_um[i])
-                    print(str(numpy.round(gvd_x[index[0][0]], 2)) + " \t" + str(numpy.round(gvd_y[index[0][0]], 3)))
-
+                    if print_for_um[i] > range_um[0] and print_for_um[i] < range_um[1]:
+                        index = numpy.where(gvd_x >= print_for_um[i])
+                        s_a = str(numpy.round(gvd_x[index[0][0]], 2))
+                        s_b = str(numpy.round(gvd_y[index[0][0]], 3))
+                        print("{0:4} {1:14}".format(s_a.rjust(4), s_b.rjust(13)))
+                    else:
+                        print("ERROR (croc.GroupVelocityDispersion.GroupVelocityDispersion): Can not print value for wavelength that is out of range of the calculation.")
             
     # plot index of refraction
     if flag_plot_n:
@@ -79,7 +111,8 @@ def GVD(material, print_for_um = [4], range_um = [1,10], material_path_mm = 0, p
 
 
 
-
+if __name__ == "__main__": 
+    GVD("caf2", range_um = [1,11], print_for_um = [2,5,8, 10.5, 12], pulse_length_fs = 0, material_path_mm = 2, flag_plot_n = False, flag_plot_gvd = False)
 
 
 
